1e129391c7bee2f119697877bec7da3f0f41da31
LaboratorioDeInformatica <labdeinf@gmail.com>
2020-11-24 15:38:42 -0300

    first commit

---
+++ src/main/java/com/labinf/poc/mongo/pocmongo/PocMongoApplication.java
@@ -0,0 +1,13 @@
+package com.labinf.poc.mongo.pocmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocMongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labinf/poc/mongo/pocmongo/PocMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labinf.poc.mongo.pocmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
